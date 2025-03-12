05f377c1bce3f61e4cdc5798bcd1f506e2f0f042
Florian <florianlloret13012@gmail.com>
2025-03-12 17:14:41 +0100

    Add exploration 3D and Dialog

---
+++ Assets/DialogueEditor/Example/ExampleInputManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DialogueEditor
+{
+    public class ExampleInputManager : MonoBehaviour
+    {
+        public KeyCode m_UpKey;
+        public KeyCode m_DownKey;
+        public KeyCode m_SelectKey;
+        public NPCConversation conversation;
+
+        private void Update()
+        {
+            if (ConversationManager.Instance != null)
+            {
+                UpdateConversationInput();
+            }
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                ConversationManager.Instance.StartConversation(conversation);
+            }
+        }
+
+        private void UpdateConversationInput()
+        {
+            if (ConversationManager.Instance.IsConversationActive)
+            {
+                if (Input.GetKeyDown(m_UpKey))
+                    ConversationManager.Instance.SelectPreviousOption();
+                else if (Input.GetKeyDown(m_DownKey))
+                    ConversationManager.Instance.SelectNextOption();
+                else if (Input.GetKeyDown(m_SelectKey))
+                    ConversationManager.Instance.PressSelectedOption();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/DialogManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace DialogueEditor
+{
+
+    [CustomEditor(typeof(DialogManager))]
+    public class DialogManagerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            DialogManager dialogManager = (DialogManager)target;
+            if (GUILayout.Button("Démarrer la conversation"))
+            {
+                if (ConversationManager.Instance != null)
+                    ConversationManager.Instance.StartConversation(dialogManager.conversation);
+                else
+                    Debug.LogWarning("ConversationManager.Instance est null !");
+            }
+        }
+    }
+    public class DialogManager : MonoBehaviour
+    {
+        public KeyCode m_UpKey;
+        public KeyCode m_DownKey;
+        public KeyCode m_SelectKey;
+        public NPCConversation conversation;
+
+        private void Update()
+        {
+            if (ConversationManager.Instance != null)
+            {
+                UpdateConversationInput();
+            }
+        }
+
+
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                ConversationManager.Instance.StartConversation(conversation);
+            }
+        }
+
+        private void UpdateConversationInput()
+        {
+            if (ConversationManager.Instance.IsConversationActive)
+            {
+                if (Input.GetKeyDown(m_UpKey))
+                    ConversationManager.Instance.SelectPreviousOption();
+                else if (Input.GetKeyDown(m_DownKey))
+                    ConversationManager.Instance.SelectNextOption();
+                else if (Input.GetKeyDown(m_SelectKey))
+                    ConversationManager.Instance.PressSelectedOption();
+            }
+        }
+    }
+}
