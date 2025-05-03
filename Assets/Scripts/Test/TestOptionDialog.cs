9e48532b7d081756646d2eab008ccb38087b3ec7
Florian <florianlloret13012@gmail.com>
2025-05-03 18:00:56 +0200

    Add Player

---
+++ Assets/Scripts/Test/TestOptionDialog.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TestOptionDialog : MonoBehaviour
+{
+    public string test;
+    public void CharismeChoisi() {
+        Debug.Log("Charisme choisi");
+    }
+    public void ForceChoisi() { 
+        Debug.Log("Force choisi");
+    }
+}
